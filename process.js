2bc664d4beee58fe7189ac1e0de63903aecf38e4
Drew Harry <drew.harry@gmail.com>
2014-04-01 16:39:21 -0400

    Messages per second with windows works.

--- process.js
+++ process.js
@@ -20,8 +20,27 @@ if(program.args.length==1) {
 
 	var log = carrier.carry(fd);
 
+	var messagesInWindow = 0;
+
+	var windowSize = 10*1000; // 10 secnds
+	var windowStartTime = -1;
 	log.on('line', function(line) {
-		console.log(line);
+		var message = JSON.parse(line);
+
+		if(windowStartTime==-1) {
+			// we're starting out.
+			windowStartTime = message.timestamp;
+			messagesInWindow = 0;
+		}
+
+		if(message.timestamp - windowStartTime > windowSize) {
+			// move to next window.
+			console.log(messagesInWindow);
+			messagesInWindow = 0;
+			windowStartTime = message.timestamp;
+		}
+
+		messagesInWindow++;
 	});
 } else {
 	console.log("No log file specified.");
